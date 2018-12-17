375c27f8b493a52215a78028190c841278f24d9e
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-12-17 15:27:13 +0700

    LatihanMVC

---
+++ LatihanMVC/src/edu/yana/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.yana.latihanmvc.event;
+
+import edu.yana.latihanmvc.model.PelangganModel;
+
+/**
+ * Nim   : 10117116
+ * Nama  : Muhammad Fitrayana
+ * Kelas : IF-3
+ */
+public interface PelangganListener {
+ 
+    public void onChange(PelangganModel pelanggan);
+}
---
+++ LatihanMVC/src/edu/yana/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.yana.latihanmvc.model;
+
+/**
+ * Nim   : 10117116
+ * Nama  : Muhammad Fitrayana
+ * Kelas : IF-3
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
